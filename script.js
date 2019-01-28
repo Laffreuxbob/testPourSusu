5e3340f190fccff9c5f24f612c8d05304b980e7b
Laffreuxbob <olivier.bricaud@sfr.fr>
2019-01-28 20:55:49 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("test");
+const mainContent = document.getElementById("mainContent");
+
+const button = document.getElementById("button");
+
+button.addEventListener( "click", () => {
+	console.log("test")
+})
