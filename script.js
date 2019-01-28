eb3a43e07b65785bd5458d73c061659ff8fed970
Unknown <olivier.bricaud@sfr.fr>
2019-01-28 22:30:15 +0100

    update scriptjs

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const mainContent = document.getElementById("mainContent");
 const button = document.getElementById("button");
 
 button.addEventListener( "click", () => {
-	console.log("test")
+	console.log("commit sur master avec les branch perso, branch1 et bob existantes")
 })
